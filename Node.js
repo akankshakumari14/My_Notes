0708a08ce9e91db28e0794586a67488c05b82882
AKanksha Kumari <132574621+akankshakumari14@users.noreply.github.com>
2026-01-30 21:02:41 +0530

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,9 @@
+-> Node.js is a runtime environment that allows JavaScript to run outside the browser.
+->🧠 Meaning of "Runtime Environment"
+
+-> A runtime environment means:
+ . It provides tools
+ . It provides libraries
+ . It provides an engine to execute code
+
+👉 Node.js uses Google Chrome's V8 Engine to run JavaScript.
